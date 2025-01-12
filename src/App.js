0a27f79ebe3701f4014758004127bb75f324d67c
Vishal-pandey17 <vishalpandey86681@gmail.com>
2025-01-12 21:01:19 +0530

    state Completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,77 @@
-
+import { useEffect, useState } from "react";
 
 function States() {
+  const [countries, setCountries] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState("");
+  
+  const [selectedState, setSelectedState] = useState("");
+  const [states, setStates] = useState([]);
+
+  const [selectedCity, setSelectedCity] = useState("");
+  const [cities, setCities] = useState([]);
+
+  const [messege, setMessege] = useState("");
+  
+  useEffect(() => {
+     const fetchCountryAPI = async () => {
+       let res =  await fetch(`https://crio-location-selector.onrender.com/countries`);
+       let data = await res.json();
+       setCountries(data);
+     }
+     fetchCountryAPI();
+  }, []);
+
+  useEffect(()=>{
+    const fetchStateAPI = async () => {
+      let res =  await fetch(`https://crio-location-selector.onrender.com/country=${selectedCountry}/states`);
+      let data = await res.json();
+      setStates(data);
+    }
+    fetchStateAPI();
+      
+  }, [selectedCountry]);
+
+  useEffect(()=>{
+
+    const fetchCityAPI = async () => {
+      let res =  await fetch(`https://crio-location-selector.onrender.com/country=${selectedCountry}/state=${selectedState}/cities`);
+      let data = await res.json();
+      setCities(data);
+    }
+    fetchCityAPI();
+
+  },[selectedState, selectedCountry]);
+
+  const handleChangeCity = (e) => {
+    setSelectedCity(e.target.value);
+    
+    setMessege(`You selected ${selectedCity}, ${selectedState}, ${selectedCountry}`);
+
+
+  }
+
   return (
     <div >
-      Hello kitty
+      <h1>Select Location</h1>
+      <select name="country" id="country" value={selectedCountry} onChange={(e)=> {setSelectedCountry(e.target.value)}}>
+          <option value={""} disabled>Select Country</option>
+          {countries.map((country)=>(
+             <option value={country}>{country}</option>
+          ))}
+      </select>
+      <select name="state" id="state" value={selectedState} disabled = {!selectedCountry} onChange={(e) => {setSelectedState(e.target.value)}}>
+          <option value="" disabled >Select State</option>
+          {states.map((state)=>(
+             <option value={state}>{state}</option>
+          ))}
+      </select>
+      <select name="city" id="city" value={selectedCity} disabled = {!selectedState} onChange={handleChangeCity}>
+          <option value="" disabled >Select City</option>
+          {cities.map((city)=>(
+             <option value={city}>{city}</option>
+          ))}
+      </select>
+          {messege && <p>{messege}</p> }
     </div>
   );
 }
